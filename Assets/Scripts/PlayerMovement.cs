7eaa0f422e032f2f60f4f9f81f09f7f6f905633c
Walker R <work.by.walker@gmail.com>
2018-09-22 14:00:05 -0500

    Still trying to animate the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     CharacterController ctrl;
+	Animator anim;
     public float moveSpeed = 0.1f;
     public float rotateSpeed = 1f;
 
 	void Start () {
         ctrl = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
 	}
 	
 	void Update () {
         if (GameLogic.Instance.mainCanvas.gameObject.activeInHierarchy)
         {
             ctrl.Move(transform.forward * Input.GetAxis("Vertical") * moveSpeed);
+		    anim.SetFloat("forwardVelocity", ctrl.velocity.magnitude);
+            Debug.Log(ctrl.velocity.x);
             transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * rotateSpeed, 0));
+            
+		    anim.SetFloat("turnVelocity", ctrl.velocity.z);
+            Debug.Log(ctrl.velocity.z);
         }
 	}
 }
