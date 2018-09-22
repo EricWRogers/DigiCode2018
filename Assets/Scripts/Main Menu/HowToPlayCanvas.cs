7ad47de160acd50691ab3a329bcaf6f3abd820c4
Walyn <walyncurtis@gmail.com>
2018-09-22 09:46:13 -0500

    How To Play UI is set up and working

--- Assets/Scripts/Main Menu/HowToPlayCanvas.cs
+++ Assets/Scripts/Main Menu/HowToPlayCanvas.cs
@@ -6,10 +6,43 @@ using UnityEngine.UI;
 public class HowToPlayCanvas : MonoBehaviour {
 
     public Canvas mainCanvas;
+    public GameObject[] display;
+    public Button back, forward;
+    int currentDisplay;
 
 	public void ButtonMainMenu()
     {
         mainCanvas.gameObject.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    public void ButtonBack()
+    {
+        if (currentDisplay > 0)
+        {
+            forward.gameObject.SetActive(true);
+            display[currentDisplay].SetActive(false);
+            currentDisplay--;
+            display[currentDisplay].SetActive(true);
+            if (currentDisplay == 0)
+            {
+                back.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void ButtonForward()
+    {
+        if (currentDisplay < display.Length-1)
+        {
+            back.gameObject.SetActive(true);
+            display[currentDisplay].SetActive(false);
+            currentDisplay++;
+            display[currentDisplay].SetActive(true);
+            if (currentDisplay == display.Length-1)
+            {
+                forward.gameObject.SetActive(false);
+            }
+        }
     }
 }
